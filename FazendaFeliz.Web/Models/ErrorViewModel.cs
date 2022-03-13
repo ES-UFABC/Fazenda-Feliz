10b66347ffd3fabaef2d9fa1a036f41a5aa7029d
Daniel <espindola.daniel@outlook.com.br>
2022-03-13 20:12:10 -0300

    Criação da estrutura base do projeto web ASP.NET Core 6

---
+++ FazendaFeliz.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FazendaFeliz.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
